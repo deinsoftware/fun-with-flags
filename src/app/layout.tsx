a2b3e44394de799d0e690c5275b884b6c461db70
Eduardo <ejrb1234@gmail.com>
2023-07-04 22:39:05 -0400

    Principal Layout modified

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,11 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import '../../styles/globals.css'
+// import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+import Head from 'next/head'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+// const inter = Inter({ subsets: ['latin']})
 
 export const metadata = {
   title: 'Fun With Flags',
@@ -15,7 +19,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Head>
+        {' '}
+        {/* ¿Esto (Head) va acá? ¿O tiene algo que ver el metadata de arriba? */}
+        <title>Fun With Flags</title>
+        <meta
+          name="description"
+          content="Time zone with country flags for events"
+        />
+        <link rel="icon" href="favicon.ico" />
+      </Head>
+
+      <body>
+        <Header />
+
+        {children}
+
+        <Footer />
+      </body>
     </html>
   )
 }
