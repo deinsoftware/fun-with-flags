cb7985d5edb1399ec788512522a12a91aeb82b99
equiman <equiman@users.noreply.github.com>
2023-07-05 01:20:12 -0500

    refactor APIs using request methods

--- src/pages/api/events.ts
+++ src/pages/api/events.ts
@@ -1,15 +1,58 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "@/utils/prisma";
 
+const getAll = async (res: NextApiResponse) => {
+    const events = await prisma.events.findMany();
+    res.status(200).json(events);
+}
+
+const getById = async (id: string, res: NextApiResponse) => {
+    const eventById = await prisma.events.findUnique({
+        where: { id }
+    });
+    return res.status(200).json(eventById);
+}
+
+const getByUserName = async (userName: string, res: NextApiResponse) => {
+    const eventByUserName = await prisma.events.findMany({
+        where: { userName }
+    });
+    res.status(200).json(eventByUserName);
+}
+
+
 const handler = async (
     req: NextApiRequest,
     res: NextApiResponse) => {
     try {
-        const events = await prisma.events.findMany()
-        res.status(200).json(events)
+        switch (req.method) {
+            case 'GET':
+                await getAll(res);
+                break
+
+            case 'POST':
+                const { id, userName } = req.body || {}
+                if (!id && !userName){
+                    res.status(400).send({ error: 'not valid parameters' })
+                }
+                if (id) await getById(id, res)
+                if (userName) await getByUserName(userName, res)
+                break
+
+            default:
+                res.status(400).send({ error: 'bad request' })
+                break
+        }
+
     } catch (err) {
-        res.status(500).send({ error: 'failed to fetch data' });
+        console.error({'API Events Error': err})
+        res.status(500).send({ error: 'failed to fetch data' })
     }
 }
 
 export default handler
+
+
+
+
+
