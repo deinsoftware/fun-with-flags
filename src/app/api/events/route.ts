f61e0aea5fabd7f6309a02859521a1f989836f7b
equiman <equiman@users.noreply.github.com>
2023-07-09 21:45:39 -0500

    add crud methods for events

--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -1,11 +1,33 @@
 import { NextResponse } from 'next/server'
+import { Prisma } from '@prisma/client'
 import prisma from '@/utils/prisma'
+import type { Events } from '@prisma/client'
 
 const getEventById = async (id: string) => {
   const eventById = await prisma.events.findUnique({
     where: { id },
   })
   return NextResponse.json(eventById)
+}
+
+const eventsSelect: Prisma.EventsSelect = {
+  id: true,
+  description: true,
+  img: true,
+  lang: true,
+  name: true,
+  tags: true,
+  timeZone: true,
+  createdAt: true,
+  updatedAt: true,
+  url: true,
+  userId: false,
+
+  Users: {
+    select: {
+      userName: true,
+    },
+  },
 }
 
 const getEventsByUserName = async (userName: string) => {
@@ -15,25 +37,7 @@ const getEventsByUserName = async (userName: string) => {
         userName,
       },
     },
-    select: {
-      id: true,
-      description: true,
-      img: true,
-      lang: true,
-      name: true,
-      tags: true,
-      timeZone: true,
-      createdAt: true,
-      updatedAt: true,
-      url: true,
-      userId: false,
-
-      Users: {
-        select: {
-          userName: true,
-        },
-      },
-    },
+    select: eventsSelect,
   })
   return NextResponse.json(eventsByUserName)
 }
@@ -53,7 +57,7 @@ const postHandler = async (request: Request) => {
     const { id, userName } = (await request.json()) || {}
     if (!id && !userName) {
       return NextResponse.json(
-        { error: 'not valid parameters' },
+        { error: 'not valid body parameters' },
         { status: 400 },
       )
     }
@@ -72,4 +76,81 @@ const postHandler = async (request: Request) => {
   }
 }
 
-export { getHandler as GET, postHandler as POST }
+const putHandler = async (request: Request) => {
+  try {
+    const { userName, ...rest } = (await request.json()) || {}
+    const data: Events = {
+      ...rest,
+      Users: {
+        connect: {
+          userName,
+        },
+      },
+    }
+
+    const event = await prisma.events.create({
+      data,
+      select: eventsSelect,
+    })
+    return NextResponse.json(event, { status: 201 })
+  } catch (error) {
+    console.error({ 'API Events Error': error })
+    return NextResponse.json({ error: 'failed to create' }, { status: 501 })
+  }
+}
+
+const patchHandler = async (request: Request) => {
+  try {
+    const { id, ...rest } = (await request.json()) || {}
+    const data: Events = {
+      ...rest,
+    }
+
+    const event = await prisma.events.update({
+      data,
+      where: { id },
+      select: eventsSelect,
+    })
+    return NextResponse.json(event, { status: 201 })
+  } catch (error) {
+    console.error({ 'API Events Error': error })
+    return NextResponse.json({ error: 'failed to create' }, { status: 501 })
+  }
+}
+
+const delHandler = async (request: Request) => {
+  try {
+    const { id } = (await request.json()) || {}
+    if (!id) {
+      return NextResponse.json(
+        { error: 'not valid body parameters' },
+        { status: 400 },
+      )
+    }
+
+    const event = await prisma.events.delete({
+      where: {
+        id,
+      },
+      select: eventsSelect,
+    })
+    return NextResponse.json(event)
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === 'P2025') {
+        return NextResponse.json({ error: 'event not found' }, { status: 404 })
+      }
+    }
+
+    console.error({ 'API Events Error': error })
+    return NextResponse.json({ error: 'failed to delete' }, { status: 501 })
+  }
+}
+
+export {
+  getHandler as GET,
+  postHandler as POST,
+  putHandler as PUT,
+  patchHandler as PATCH,
+  delHandler as DELETE,
+}
