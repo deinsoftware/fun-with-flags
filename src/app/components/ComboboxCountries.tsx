4885ca59c74e3bb288a5bbb3fdb20f1c33b3425f
Alexis033 <10guevarafm1102@gmail.com>
2023-07-12 23:31:59 -0500

    separate today dates from next day

--- src/app/components/ComboboxCountries.tsx
+++ src/app/components/ComboboxCountries.tsx
@@ -4,6 +4,7 @@ import style from './ComboboxCountries.module.css'
 
 import { getDatesList, sortDatesList } from "@/helpers/events"
 import { EventDate, ZoneList } from "@/helpers/events.types"
+import { useEffect, useState } from "react"
 
 type TimezoneInfo = {
     [time: string]: {
@@ -12,6 +13,14 @@ type TimezoneInfo = {
         date: string;
     }
   };
+
+type DateArray = [
+    string, {
+        countryCodes: [string[]];
+        gmt?: string | undefined;
+        date: string;
+    }
+ ]
 
 const data = `{
     "id": "64a25c08492a2b3680a10500",
@@ -25,10 +34,6 @@ const data = `{
     ],
     "timeZone": {
         "list": [
-            {
-                "countryCode": "CO",
-                "name": "America/Bogota"
-            },
             {
                 "countryCode": "CO",
                 "name": "America/Bogota"
@@ -88,48 +93,75 @@ const ComboboxCountries = () => {
         timeFormat: 24,
     }
     const dateList =sortDatesList(getDatesList(valueList))
-    // console.log(dateList)
-    const filterDates = (dateList: EventDate[]) =>{
-        let groupedDates: TimezoneInfo = {}
 
-        dateList?.forEach((dateInfo) => {
-            const countryCode: string = dateInfo.countryCode
-            const time: string = dateInfo.i18n.time
-            const date = dateInfo.i18n.date
-            const gmt = dateInfo.gmt
-            const name = dateInfo.name
+    const [filteredDates, setFilteredDates] = useState<DateArray[]>([])
+    const [isNextDate, setIsNextDate] = useState<boolean>(false)
+    const currentDatePlusOne = new Date(currentDate)
+    currentDatePlusOne.setDate(currentDatePlusOne.getDate() + 1)
 
-            if (groupedDates[time]){
-                groupedDates[time].countryCodes.push([countryCode, name])
-            } else {
-                groupedDates[time] = {
-                    countryCodes: [[countryCode, name]],
-                    gmt,
-                    date,
-                }
-            }})
-        const groupedDatesArray = Object.entries(groupedDates)
+    useEffect(()=>{
+        const filterDates = (dateList: EventDate[]): DateArray[] =>{
+            let groupedDates: TimezoneInfo = {}
+    
+            dateList?.forEach((dateInfo) => {
+                const countryCode: string = dateInfo.countryCode
+                const time: string = dateInfo.i18n.time
+                const date = dateInfo.i18n.date
+                const gmt = dateInfo.acronym
+                const name = dateInfo.name
+    
+                if (groupedDates[time]){
+                    groupedDates[time].countryCodes.push([countryCode, name])
+                } else {
+                    groupedDates[time] = {
+                        countryCodes: [[countryCode, name]],
+                        gmt,
+                        date,
+                    }
+                }})
+            const groupedDatesArray = Object.entries(groupedDates)
+            return groupedDatesArray
+        } 
+        setFilteredDates(filterDates(dateList))  
+    }, [])  
 
-        const timeToRender = groupedDatesArray.map(([time, groupedDate]) => {
+    useEffect(()=>{
+        const thereIsNextDate = (datesArray: DateArray[], currentDate: Date)=>{
+            let nextDate = false
+            const validation = datesArray.some(([, groupedDate]) => {
+                const date= new Date(groupedDate.date)
+                const isNextDate = date.getDate() > currentDate.getDate()
+                return isNextDate 
+            })
+            setIsNextDate(validation)
+        }  
+        thereIsNextDate(filteredDates, currentDate)
+    }, [filteredDates])
+
+    
+    const datesToRender =(datesArray: DateArray[], nextDate: boolean= false )=>{
+        const timeToRender = datesArray.map(([time, groupedDate]) => {
             const date= new Date(groupedDate.date)
             const isSameDate = date.toDateString() === currentDate.toDateString()
             const isNextDate = date.getDate() > currentDate.getDate() 
             const isPrevDate = date.getDate() < currentDate.getDate() 
-            return (
-                <div key={time} className={style['countries']}>
-                    {isNextDate && <p >{`Next day`}</p>}
-                    {isPrevDate && <p >{`Previous day`}</p>}
-                    <p className={style['time']}>{`${time}`}</p>
-                    <p className={style['gmt']}>{`(${groupedDate.gmt})`}</p>
-                    <div className={style['flags-container']}>
-                    {groupedDate.countryCodes.map(([countryCode, name]) =>{
+
+            const handleClick = (event: React.MouseEvent<HTMLImageElement>)=>{
+                const target = event.target as HTMLImageElement
+                console.log(target.id)
+            }
+            const elementJSX = ()=>{
+                return (
+                    <>
+                        <p className={style['time']}>{`${time}`}</p>
+                        <p className={style['gmt']}>{`(${groupedDate.gmt})`}</p>
+                        <div className={style['flags-container']}>
+                        {groupedDate.countryCodes.map(([countryCode, name]) =>{
                         return (
-                            <div key={name} className={style['flag']}>
+                            <div key={self.crypto.randomUUID()} className={style['flag']}>
                                 
                                 <ReactCountryFlag
-                                onClick={() => {
-                                    console.log(name)
-                                }} 
+                                onClick={handleClick} 
                                 id={name}
                                 countryCode={countryCode}
                                 svg
@@ -139,23 +171,39 @@ const ComboboxCountries = () => {
                                 }}
                                 />
                             </div>
-                        )
-                    })}
+                            )
+                        })}
                     </div>
+                    </>
+                    
+                )
+            }
+            
+            return (
+                <div key={time} className={style['countries']}>
+                    {!nextDate && isSameDate && elementJSX()}              
+                    {nextDate && isNextDate && elementJSX()} 
                 </div>
             )
         })
         
         return  timeToRender
-    } 
+    }
 
     return (
         
         <div className={style['countries-container']}>
             <p><strong>{currentDate.toDateString()}</strong></p>
-            {(typeof dateList !== 'undefined') ?  filterDates(dateList)
+            {(typeof dateList !== 'undefined') ?  datesToRender(filteredDates) 
             : 'Add a timezone to start'}
+
             
+            {isNextDate && 
+             <>
+                <p><strong>{currentDatePlusOne.toDateString()}</strong></p>
+                {datesToRender(filteredDates, true)}
+             </> 
+            }
         </div>     
         
     )
