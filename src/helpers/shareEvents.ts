31805ce74ec177cca24a953c0a67c761d8f48852
Alexis033 <10guevarafm1102@gmail.com>
2023-07-19 14:41:05 -0500

    updated twitter limit characters

--- src/helpers/shareEvents.ts
+++ src/helpers/shareEvents.ts
@@ -8,13 +8,14 @@ const shareEvents = ({
   hashtags?: string[]
 }) => {
   const twitterBaseURL = 'https://twitter.com/intent/tweet'
+  const twitterCharacterLimit = 280
 
   const twitterURL = new URL(twitterBaseURL)
 
   twitterURL.searchParams.append('url', url)
 
-  if (text.length > 140) {
-    text = text.substring(0, 137) + '...'
+  if (text.length > twitterCharacterLimit - url.length) {
+    text = text.substring(0, twitterCharacterLimit - url.length - 3) + '...'
   }
   twitterURL.searchParams.append('text', text)
   if (hashtags) twitterURL.searchParams.append('hashtags', hashtags.toString())
