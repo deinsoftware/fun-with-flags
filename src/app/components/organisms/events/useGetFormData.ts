972218ccbefd7023ed05a6f11b863b19c1e0ddec
Eduardo <ejrb1234@gmail.com>
2023-08-09 16:12:28 -0400

    test - date + time from context

--- src/app/components/organisms/events/useGetFormData.ts
+++ src/app/components/organisms/events/useGetFormData.ts
@@ -1,8 +1,49 @@
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
+
+import { TimeZoneContext } from '@/app/context/timeZoneContext'
 
 export const useGetFormData = () => {
-  const currentTime = new Date().toTimeString().slice(0, 5)
-  const currentDate = new Date().toISOString().slice(0, 10)
+  const { timeZones } = useContext(TimeZoneContext)
+
+  // Función que saque el date (xxxx-xx-xx)
+  // Función que saque el time (xx:xx)
+  // Ambas, de un:
+  // new Date().toISOString() // '2023-08-09T16:47:45.835Z'
+  //                              yyyy-mm-dd | hh:mm
+
+  const originDate = timeZones.origin.date
+
+  // const currentDate = originDate.slice(0, 10)
+  const currentTime = originDate.slice(11, 16)
+
+  // const currentDate = originDate.split('T')[0]
+  // const currentTime =
+  //   originDate.split('T')[1].split(':')[0] +
+  //   ':' +
+  //   originDate.split('T')[1].split(':')[1]
+
+  const getCurrentDate = () => {
+    const defaultDate = new Date(originDate)
+
+    const year = defaultDate.getFullYear()
+    const month = defaultDate.getMonth() + 1
+    const day = defaultDate.getDate()
+
+    let monthString, dayString
+
+    if (month < 10) {
+      monthString = `0${month}`
+    }
+    if (day < 10) {
+      dayString = `0${day}`
+    }
+
+    const currentDate = `${year}-${monthString}-${dayString}`
+
+    return currentDate
+  }
+
+  const currentDate = getCurrentDate()
 
   const [formData, setFormData] = useState({
     eventName: '',
@@ -14,6 +55,7 @@ export const useGetFormData = () => {
     image: '',
     combo: '',
   })
+
   useEffect(() => {
     const getInitialFormData = () => {
       const localFormData = localStorage.getItem('formData')
