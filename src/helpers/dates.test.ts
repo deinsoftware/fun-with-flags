f2b1a379f28922606ea0bae9396efd5b509a5a52
equiman <equiman@users.noreply.github.com>
2023-08-10 17:23:39 -0500

    Revert "add build verification in other OS"
    This reverts commit 210f8dd391dcd1f0486e3ddda77ffb9bc8831f59.

--- src/helpers/dates.test.ts
+++ src/helpers/dates.test.ts
@@ -1,21 +1,6 @@
 import { it, expect, describe, vi } from 'vitest'
 
-import {
-  isValidTimeZone,
-  convertGmtToNumber,
-  getRegionNames,
-  extractDate,
-  extractTime,
-  joinISODate,
-} from './dates'
-
-beforeEach(() => {
-  vi.useFakeTimers()
-})
-
-afterEach(() => {
-  vi.useRealTimers()
-})
+import { isValidTimeZone, convertGmtToNumber, getRegionNames } from './dates'
 
 describe('isValidTimeZone()', () => {
   it('should return true when using a correct time zone', () => {
@@ -39,65 +24,6 @@ describe('isValidTimeZone()', () => {
     expect(result).toBeFalsy()
 
     spy.mockRestore()
-  })
-})
-
-describe('extractDate()', () => {
-  it('should return the correct date in YYYY-MM-DD format', () => {
-    const date = new Date(2023, 11, 31)
-    vi.setSystemTime(date)
-
-    const result = extractDate(date)
-    expect(result).toEqual('2023-12-31')
-  })
-
-  it('should pad single-digit date ans seconds with leading 0', () => {
-    const date = new Date(2022, 0, 2)
-    vi.setSystemTime(date)
-    const result = extractDate(date)
-    expect(result).toEqual('2022-01-02')
-  })
-})
-
-describe('extractTime()', () => {
-  it('should return the correct time in HH:MM:SS format', () => {
-    const date = new Date('2022-01-01T12:34:56')
-    vi.setSystemTime(date)
-    const result = extractTime(date)
-    expect(result).toBe('12:34:56')
-  })
-
-  it('should pad single-digit hour, minute, and second with leading 0', () => {
-    const date = new Date('2022-01-01T01:02:03')
-    vi.setSystemTime(date)
-    const result = extractTime(date)
-    expect(result).toBe('01:02:03')
-  })
-})
-
-describe('joinISODate()', () => {
-  it('should return a valid ISO string when all parameters are provided', () => {
-    const result = joinISODate('2022-01-01', '12:00:00', '+03:00')
-    expect(result).toEqual('2022-01-01T09:00:00.000Z')
-  })
-
-  it('should return a valid ISO string when only date and time are provided', () => {
-    const result = joinISODate('2022-01-01', '12:00:00')
-    expect(result).toEqual('2022-01-01T12:00:00.000Z')
-  })
-
-  it('should return an exception when creating the date fails', () => {
-    const result = () => {
-      joinISODate('2022-01-01', '')
-    }
-    expect(result).toThrowError()
-  })
-
-  it('should return an exception when gmt is not valid', () => {
-    const result = () => {
-      joinISODate('2022-01-01', '12:00:00', '5:00')
-    }
-    expect(result).toThrowError()
   })
 })
 
--- src/helpers/dates.ts
+++ src/helpers/dates.ts
@@ -15,27 +15,6 @@ export const isValidTimeZone = (timeZone: TimeZones): boolean => {
   }
 }
 
-export const extractDate = (date: Date) => {
-  let year = date.getFullYear()
-  let month = String(date.getMonth() + 1).padStart(2, '0')
-  let day = String(date.getDate()).padStart(2, '0')
-
-  return `${year}-${month}-${day}`
-}
-
-export const extractTime = (date: Date) => {
-  const hour = String(date.getHours()).padStart(2, '0')
-  const minute = String(date.getMinutes()).padStart(2, '0')
-  const second = String(date.getSeconds()).padStart(2, '0')
-
-  return `${hour}:${minute}:${second}`
-}
-
-export const joinISODate = (date: string, time: string, gmt: string = 'Z') => {
-  const isoDate = `${date}T${time}${gmt}`
-  return new Date(isoDate).toISOString()
-}
-
 export const getDate = (options: { timeZone: TimeZones }, originDate: Date) => {
   const date = Intl.DateTimeFormat('default', {
     ...options,
@@ -46,10 +25,8 @@ export const getDate = (options: { timeZone: TimeZones }, originDate: Date) => {
     ?.formatToParts(originDate)
     ?.reverse()
 
-  const DAY_INDEX = 2
-  const MONTH_INDEX = 4
   // swap day and month
-  ;[date[DAY_INDEX], date[MONTH_INDEX]] = [date[MONTH_INDEX], date[DAY_INDEX]]
+  ;[date[2], date[4]] = [date[4], date[2]]
 
   return date.reduce(
     (string, part) => string + (part.type === 'literal' ? '-' : part.value),
