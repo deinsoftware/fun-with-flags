71c1a9327ffa2ee630c9c4610fee84324443386b
Eduardo <ejrb1234@gmail.com>
2023-08-14 17:01:59 -0400

    Added loader image

--- src/app/components/atoms/util/loading/Loading.tsx
+++ src/app/components/atoms/util/loading/Loading.tsx
@@ -1,7 +1,18 @@
+import Image from 'next/image'
+
+import styles from './Loading.module.css'
+
 const Loading = () => {
   return (
     <>
-      <div>Loading...</div>
+      <section className={styles['container-loader']}>
+        <Image
+          alt="Loading"
+          height={100}
+          src="/img/ui/loader.svg"
+          width={100}
+        />
+      </section>
     </>
   )
 }
