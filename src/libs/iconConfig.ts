e66a1bc50bd01476a2224600b8d81d5f0f651109
Eduardo <ejrb1234@gmail.com>
2023-08-15 15:13:11 -0400

    new props in libs/IconConfig

--- src/libs/iconConfig.ts
+++ src/libs/iconConfig.ts
@@ -1,7 +1,18 @@
 export const lucidIcons = {
   size: 24,
   color: {
+    main: '#7E56DA',
     dark: '#454545',
+    light: '#FF0000',
+  },
+  strokeWidth: 2,
+}
+
+export const lucidIconsTimePicker = {
+  size: 22,
+  color: {
+    main: '#7E56DA',
+    dark: '#FEFEFE',
     light: '#FF0000',
   },
   strokeWidth: 2,
