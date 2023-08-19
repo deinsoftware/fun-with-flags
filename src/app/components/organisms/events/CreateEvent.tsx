d894398fdc98ee5e6c572fccb019474455d501a2
equiman <equiman@users.noreply.github.com>
2023-08-19 16:02:48 -0500

    fix combo message when using acronyms

--- src/app/components/organisms/events/CreateEvent.tsx
+++ src/app/components/organisms/events/CreateEvent.tsx
@@ -47,7 +47,6 @@ import { lucidIcons } from '@/libs/icon-config'
 import { createEvent } from '@/services/event'
 import { EventBody } from '@/types/event.types'
 import { toastIconTheme, toastStyle } from '@/libs/react-host-toast-config'
-import { DatePattern } from '@/types/dates.types'
 
 const CreateEvent = () => {
   const [isOpenSelectTimeZone, setIsOpenSelectTimeZone] = useState(false)
