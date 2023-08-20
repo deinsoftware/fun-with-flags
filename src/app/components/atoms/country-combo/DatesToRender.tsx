2c649c97ef65ee5a2da8ccca1dd26ee20e82b1b7
Alexis033 <10guevarafm1102@gmail.com>
2023-08-20 00:13:18 -0500

    Refactor DatesToRender and add props Combobox

--- src/app/components/atoms/country-combo/DatesToRender.tsx
+++ src/app/components/atoms/country-combo/DatesToRender.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useEffect, useRef, useState, RefObject } from 'react'
+import { useEffect, useRef, useState, RefObject, useCallback } from 'react'
 
 import ReactCountryFlag from 'react-country-flag'
 
@@ -9,17 +9,86 @@ import { useTimeZoneContext } from '@/app/context/useTimeZoneContext'
 import { DatesFilteredArray } from '@/types/flags.types'
 import { Countries } from '@/types/countries.types'
 import { Timezones } from '@/types/timezones.types'
+import { EventDate, Zone } from '@/helpers/events.types'
 
 type Props = {
   datesArray: DatesFilteredArray[]
   getTextContent: (ref: RefObject<HTMLDivElement> | null) => void
+  showGmt?: boolean
+  showGmtWord?: boolean
+  showHourComplete?: boolean
 }
 
-export const DatesToRender = ({ datesArray, getTextContent }: Props) => {
+export const DatesToRender = ({
+  datesArray,
+  getTextContent,
+  showGmt,
+  showGmtWord,
+  showHourComplete,
+}: Props) => {
   const { deleteTimeZone, format } = useTimeZoneContext()
   const [timeToRender, setTimeToRender] = useState<React.ReactNode[]>([])
 
   const ref = useRef<HTMLDivElement>(null)
+
+  const handleClick = useCallback(
+    (event: React.MouseEvent<HTMLImageElement>) => {
+      const target = event.target as HTMLImageElement
+      const name = target.id.split('--')[0] as Timezones
+      const countryCode = target.id.split('--')[1] as Countries
+      deleteTimeZone({ countryCode, name })
+    },
+    [deleteTimeZone],
+  )
+  const getTimeInfo = useCallback(
+    (gmt: string, countries: EventDate[], complementShortHour: string) => {
+      let classNameToAdd = ''
+      if (!showHourComplete) {
+        classNameToAdd =
+          format === 12 ? style['time-short-12'] : style['time-short-24']
+      }
+      const timeText = showHourComplete
+        ? countries[0].i18n.time
+        : `${countries[0].i18n.time.split(':')[0]} ${complementShortHour}`
+
+      return (
+        <div key={gmt} className={style['countries']}>
+          <p
+            className={`${
+              format === 12 ? style['time-12'] : style['time-24']
+            } ${classNameToAdd}`}
+          >
+            {timeText}
+          </p>
+          {(showGmt || showGmtWord) && (
+            <p className={style['gmt']}>{`(${showGmtWord ? 'GMT' : ''}${
+              showGmt ? gmt : ''
+            })`}</p>
+          )}
+          <div className={style['flags-container']}>
+            {countries.map(({ countryCode, name }) => (
+              <div key={self.crypto.randomUUID()} className={style['flag']}>
+                <ReactCountryFlag
+                  svg
+                  alt={`Flag of ${countryCode}`}
+                  countryCode={countryCode}
+                  id={`${name}--${countryCode}`}
+                  style={{ width: '1.6rem', height: '1.6rem' }}
+                  title={`Flag of ${countryCode}`}
+                  onClick={handleClick}
+                />
+                <ReactCountryFlag
+                  countryCode={countryCode}
+                  style={{ display: 'none' }}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      )
+    },
+    [showHourComplete, showGmt, showGmtWord, format, handleClick],
+  )
 
   useEffect(() => {
     getTextContent(ref)
@@ -30,48 +99,13 @@ export const DatesToRender = ({ datesArray, getTextContent }: Props) => {
 
   useEffect(() => {
     const result = datesArray.map(([date, groupedCountries]) => {
-      const handleClick = (event: React.MouseEvent<HTMLImageElement>) => {
-        const target = event.target as HTMLImageElement
-        const name = target.id.split('--')[0] as Timezones
-        const countryCode = target.id.split('--')[1] as Countries
-        deleteTimeZone({ countryCode, name })
-      }
-      const groupedCountriesArray = Object.entries(groupedCountries)
-      const timeInfo = groupedCountriesArray.map(([gmt, countries]) => {
-        return (
-          <div key={gmt} className={style['countries']}>
-            <p className={format === 12 ? style['time-12'] : style['time-24']}>
-              {`${countries[0].i18n.time}`}
-            </p>
-            <p className={style['gmt']}>{`(GMT${gmt})`}</p>
-            <div className={style['flags-container']}>
-              {countries.map(({ countryCode, name }) => {
-                return (
-                  <div key={self.crypto.randomUUID()} className={style['flag']}>
-                    <ReactCountryFlag
-                      svg
-                      alt={`Flag of ${countryCode}`}
-                      countryCode={countryCode}
-                      id={`${name}--${countryCode}`}
-                      style={{
-                        width: '1.6rem',
-                        height: '1.6rem',
-                      }}
-                      title={`Flag of ${countryCode}`}
-                      onClick={handleClick}
-                    />
-                    <ReactCountryFlag
-                      countryCode={countryCode}
-                      style={{ display: 'none' }}
-                    />
-                  </div>
-                )
-              })}
-              {`\n`}
-            </div>
-          </div>
-        )
-      })
+      const values = Object.values(groupedCountries)
+      const firstCountry = values[0][0]
+      const complementShortHour: string =
+        format === 12 ? firstCountry.i18n.time.split(' ')[1] : 'H'
+      const timeInfo = Object.entries(groupedCountries).map(
+        ([gmt, countries]) => getTimeInfo(gmt, countries, complementShortHour),
+      )
 
       return (
         <div
@@ -82,11 +116,11 @@ export const DatesToRender = ({ datesArray, getTextContent }: Props) => {
           <p>
             <strong>{date}</strong>
           </p>
-          {`\n`}
           {timeInfo}
         </div>
       )
     })
+
     setTimeToRender(result)
   }, [datesArray, deleteTimeZone, format])
 
--- src/app/components/molecules/country-combo/ComboboxCountries.tsx
+++ src/app/components/molecules/country-combo/ComboboxCountries.tsx
@@ -18,9 +18,18 @@ import { lucidIcons } from '@/libs/icon-config'
 type Props = {
   getTextContent: (ref: RefObject<HTMLDivElement> | null) => void
   handleAddCountry: (value: SetStateAction<boolean>) => void
+  showGmt?: boolean
+  showGmtWord?: boolean
+  showHourComplete?: boolean
 }
 
-const ComboboxCountries = ({ getTextContent, handleAddCountry }: Props) => {
+const ComboboxCountries = ({
+  getTextContent,
+  handleAddCountry,
+  showGmt = true,
+  showGmtWord = true,
+  showHourComplete = true,
+}: Props) => {
   const { format } = useTimeZoneContext()
   const { dateList } = useGetInfoDates({ format })
   const filteredDates = useFilteredDates(dateList, format)
@@ -33,6 +42,9 @@ const ComboboxCountries = ({ getTextContent, handleAddCountry }: Props) => {
         <DatesToRender
           datesArray={filteredDates}
           getTextContent={getTextContent}
+          showGmt={showGmt}
+          showGmtWord={showGmtWord}
+          showHourComplete={showHourComplete}
         />
       )}
       <div className={style['add-button-container']}>
