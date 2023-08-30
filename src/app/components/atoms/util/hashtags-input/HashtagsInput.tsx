77524453e18316c934fca9ac846cf8549f3bff00
Eduardo <ejrb1234@gmail.com>
2023-08-30 17:45:06 -0400

    refactor(app): hasgtags input refactor functions + styles
    Refactoring some functions and adding styles to HastagInput

--- src/app/components/atoms/util/hashtags-input/HashtagsInput.tsx
+++ src/app/components/atoms/util/hashtags-input/HashtagsInput.tsx
@@ -4,6 +4,8 @@ import { toast } from 'react-hot-toast'
 
 import { type KeyboardEvent, useState } from 'react'
 
+import { X } from 'lucide-react'
+
 import styles from './HashtagsInput.module.css'
 
 type Props = {
@@ -16,22 +18,37 @@ const Hashtags = ({ hashTagsList, addHashtag, removeHashtag }: Props) => {
   const initialState = ''
   const [tag, setTag] = useState(initialState)
 
+  const validateHashTag = (hashTag: string) => {
+    let trimmedHashTag = hashTag.trim().replaceAll('#', '')
+
+    if (!trimmedHashTag) {
+      return 'Hashtag is required'
+    }
+
+    if (trimmedHashTag.includes(' ')) {
+      return 'Spaces are not allowed'
+    }
+
+    const regExp = /[^A-Za-z0-9#]/
+    if (regExp.test(trimmedHashTag)) {
+      return 'Special characters are not allowed'
+    }
+
+    if (hashTagsList.includes(trimmedHashTag)) {
+      return 'Hashtag already exists'
+    }
+
+    return null
+  }
+
   const onKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
     const target = event.target as HTMLInputElement
     let hashTag = target.value.trim().replaceAll('#', '')
 
     if (event.key === 'Enter' && hashTag) {
-      if (hashTag.includes(' ')) {
-        return toast('Spaces are not allowed')
-      }
-
-      const regExp = /[^A-Za-z0-9#]/
-      if (regExp.test(hashTag)) {
-        return toast('Special characters are not allowed')
-      }
-
-      if (hashTagsList.includes(hashTag)) {
-        return toast('Hashtag already exists')
+      const validationError = validateHashTag(hashTag)
+      if (validationError) {
+        return toast(validationError)
       }
 
       addHashtag(hashTag)
@@ -39,28 +56,51 @@ const Hashtags = ({ hashTagsList, addHashtag, removeHashtag }: Props) => {
     }
   }
 
+  const handleClick = (tag: string) => {
+    let hashTag = tag.trim().replaceAll('#', '')
+
+    const validationError = validateHashTag(hashTag)
+    if (validationError) {
+      return toast(validationError)
+    }
+
+    addHashtag(hashTag)
+    setTag(initialState)
+  }
+
   return (
     <>
       <div className={styles['hashtag-container']}>
-        <input
-          className={styles['hashtag-input']}
-          placeholder="Hashtags (add pressing enter)"
-          type="text"
-          value={tag}
-          onChange={(e) => setTag(e.target.value)}
-          onKeyDown={onKeyDown}
-        />
+        <div className={styles['actions']}>
+          <input
+            className={styles['hashtag-input']}
+            placeholder="Hashtags (add pressing enter)"
+            type="text"
+            value={tag}
+            onChange={(e) => setTag(e.target.value)}
+            onKeyDown={onKeyDown}
+          />
+          <button
+            className={styles['add-hashtag']}
+            type="button"
+            onClick={() => handleClick(tag)}
+          >
+            Add
+          </button>
+        </div>
 
         {hashTagsList.length > 0 && (
           <div className={styles['tags-container']}>
             {hashTagsList.map((hashTag) => (
-              <span
-                key={hashTag}
-                className={styles['tag']}
-                onClick={() => removeHashtag(hashTag)}
-              >
-                {`#${hashTag}`}
-              </span>
+              <div key={hashTag} className={styles['tag']}>
+                <span className={styles['tag-text']}>{`#${hashTag}`}</span>
+                <i
+                  className={styles['remove-hashtag-icon']}
+                  onClick={() => removeHashtag(hashTag)}
+                >
+                  <X color="#fc0000" size={18} /> {/*TODO: Crear constantes*/}
+                </i>
+              </div>
             ))}
           </div>
         )}
