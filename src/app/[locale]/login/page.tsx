209b99edd831742385f1cebe21e35e6c5cbac118
Eduardo <ejrb1234@gmail.com>
2023-09-06 01:49:11 -0400

    fix(app): Added max-width in LoginPage
    Added max-width in LoginPage

--- src/app/[locale]/login/page.tsx
+++ src/app/[locale]/login/page.tsx
@@ -3,9 +3,10 @@
 import Link from 'next/link'
 import Image from 'next/image'
 
-
 import { signIn } from 'next-auth/react'
 import { useSearchParams } from 'next/navigation'
+
+import TitleOnPage from '../components/atoms/ui/TitleOnPage'
 
 import styles from './page.module.css'
 
@@ -15,42 +16,46 @@ const LoginPage = () => {
 
   return (
     <>
-      <div className={styles['login-container']}>
-        <div className="">
-          <span className={styles['paragraph']}>
-            {`Use your email or another service to continue with FwF (it's free)!`}
-          </span>
-        </div>
-
-        <div className={styles['providers-button']}>
-          <button
-            className={`${styles['provider']} ${styles['google-provider']}`}
-            onClick={() => signIn('google', { callbackUrl })}
-          >
-            <Image
-              alt="Login with Google"
-              className={styles['image-provider']}
-              height={32}
-              src="/img/auth/google.svg"
-              width={32}
-            />
-            <span className={styles['name-provider']}>
-              Continue with Google
+      <div className={styles['container']}>
+        <TitleOnPage>Sign in to Fun With Flags</TitleOnPage>
+        {/* TODO: Este título quemado se pasará a i18n después de aceptar el PR que contiene el objeto de los textos quemados de abajo */}
+        <div className={styles['login-container']}>
+          <div className="">
+            <span className={styles['paragraph']}>
+              {`Use your email or another service to continue with FwF (it's free)!`}
             </span>
-          </button>
-        </div>
+          </div>
 
-        <div className="">
-          <span className={styles['helper-text']}>
-            {`By continuing, you agree to FwF's`}{' '}
-            <Link className={styles['helper-text-link']} href="#">
-              Terms of Use.
-            </Link>{' '}
-            Read our{' '}
-            <Link className={styles['helper-text-link']} href="#">
-              Privacy Policy.
-            </Link>
-          </span>
+          <div className={styles['providers-button']}>
+            <button
+              className={`${styles['provider']} ${styles['google-provider']}`}
+              onClick={() => signIn('google', { callbackUrl })}
+            >
+              <Image
+                alt="Login with Google"
+                className={styles['image-provider']}
+                height={32}
+                src="/img/auth/google.svg"
+                width={32}
+              />
+              <span className={styles['name-provider']}>
+                Continue with Google
+              </span>
+            </button>
+          </div>
+
+          <div className="">
+            <span className={styles['helper-text']}>
+              {`By continuing, you agree to FwF's`}{' '}
+              <Link className={styles['helper-text-link']} href="#">
+                Terms of Use.
+              </Link>{' '}
+              Read our{' '}
+              <Link className={styles['helper-text-link']} href="#">
+                Privacy Policy.
+              </Link>
+            </span>
+          </div>
         </div>
       </div>
     </>
