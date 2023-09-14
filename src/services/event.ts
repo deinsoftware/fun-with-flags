d1e4b72b807302736b9f3ca61d407718275110c5
Alexis033 <10guevarafm1102@gmail.com>
2023-09-13 20:06:54 -0500

    feat(app): service update event

--- src/services/event.ts
+++ src/services/event.ts
@@ -35,3 +35,37 @@ export async function createEvent(
     return result
   }
 }
+
+export async function updateEvent(
+  body: Prisma.EventsUpdateInput & { id: string },
+  signal?: AbortSignal,
+) {
+  const headers = new Headers()
+  headers.append('Content-Type', 'application/json')
+
+  const payload = JSON.stringify(body)
+  const params: RequestInit = {
+    method: 'PATCH',
+    headers,
+    body: payload,
+    redirect: 'follow',
+    signal,
+  }
+  try {
+    const response = await fetch(`${NEXT_PUBLIC_API_URL}/api/events`, params)
+    const { ok, status } = response ?? {}
+    const result = { ok, status, data: {} }
+    if (response.ok) {
+      result.data = await response.json()
+    }
+    return result
+  } catch (error) {
+    console.error(error)
+    const result = {
+      ok: false,
+      status: 500,
+      data: {},
+    }
+    return result
+  }
+}
