bea76efce9551390d6cbb78b4c3d6c5fa86c42d4
Eduardo <ejrb1234@gmail.com>
2023-10-12 22:36:59 -0400

    services getEventsByUserName

--- src/services/event.ts
+++ src/services/event.ts
@@ -69,3 +69,40 @@ export async function updateEvent(
     return result
   }
 }
+
+export async function getEventsByUserName(
+  body: {
+    userName: string
+  },
+  signal?: AbortSignal,
+) {
+  const headers = new Headers()
+  headers.append('Content-Type', 'application/json')
+
+  const payload = JSON.stringify(body)
+  const params: RequestInit = {
+    method: 'POST',
+    headers,
+    body: payload,
+    redirect: 'follow',
+    signal,
+  }
+
+  try {
+    const response = await fetch(`${NEXT_PUBLIC_API_URL}/api/events`, params)
+    const { ok, status } = response ?? {}
+    const result = { ok, status, data: {} }
+    if (response.ok) {
+      result.data = await response.json()
+    }
+    return result
+  } catch (error) {
+    console.error(error)
+    const result = {
+      ok: false,
+      status: 500,
+      data: {},
+    }
+    return result
+  }
+}
