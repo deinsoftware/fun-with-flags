8a2022b7d2a0e35525c24ad0e4b2e19785dd2556
Eduardo <ejrb1234@gmail.com>
2023-10-17 16:10:04 -0400

    moved myEventsPage to client rendering

---
+++ src/app/[locale]/components/organisms/events/my-events/MyEvents.tsx
@@ -0,0 +1,72 @@
+'use client'
+
+import { useTranslations } from 'next-intl'
+
+import { useSession } from 'next-auth/react'
+import { useEffect, useState } from 'react'
+
+import styles from './MyEvents.module.css'
+
+import Title from '@/components/atoms/ui/Title'
+import { UserEvents } from '@/components/user-events/UserEvents'
+import { getEventsByUserName } from '@/services/event'
+
+export const MyEvents = () => {
+  const t = useTranslations('Events')
+  const { data: session } = useSession()
+
+  const [signal, setSignal] = useState<AbortSignal>()
+  const [userName, setUserName] = useState('')
+  const [myEvents, setMyEvents] = useState([])
+
+  useEffect(() => {
+    const username = session?.user?.name
+    setUserName(username as string)
+  }, [session])
+
+  useEffect(() => {
+    if (!userName) return
+
+    const body = {
+      userName: userName,
+    }
+
+    // please, don't use any type in the newData 😂
+    getEventsByUserName(body, signal).then((newData: any) => {
+      setMyEvents(newData.data)
+      //   console.log(newData)
+    })
+  }, [userName])
+
+  //   useEffect(() => {
+  //     const controller = new AbortController()
+  //     const signal = controller.signal
+  //     setSignal(signal)
+
+  //     return () => {
+  //       controller.abort()
+  //     }
+  //   }, [])
+
+  return (
+    <>
+      <div className={styles['container-events']}>
+        <Title>{t('title')}</Title>
+        <main className={styles['events']}>
+          {myEvents?.map((event: any) => {
+            return (
+              <UserEvents
+                key={event.id}
+                date={event.createdAt}
+                description={event.description}
+                name={event.name}
+                timeZone={event.timeZone}
+                url={event.url}
+              />
+            )
+          })}
+        </main>
+      </div>
+    </>
+  )
+}
--- src/app/[locale]/events/page.tsx
+++ src/app/[locale]/events/page.tsx
@@ -1,17 +1,8 @@
 import { getTranslator } from 'next-intl/server'
 
-import { useTranslations } from 'next-intl'
-
-import { useSession } from 'next-auth/react'
-
-import Title from '../components/atoms/ui/Title'
-
-import styles from './page.module.css'
+import { MyEvents } from '@/components/organisms/events/my-events/MyEvents'
 
 import { MetadataProps } from '@/app/layout.types'
-
-import { UserEvents } from '@/components/user-events/UserEvents'
-import { getEventsByUserName } from '@/services/event'
 
 export const generateMetadata = async ({
   params: { locale },
@@ -23,47 +14,10 @@ export const generateMetadata = async ({
   }
 }
 
-const controller = new AbortController()
-const signal = controller.signal
-controller.abort()
-
 const EventsPage = async () => {
-  // const t = useTranslations('Events')
-  // const { data: session } = useSession()
-  const body = {
-    // userName: session?.user?.name as string,
-    userName: 'ejrb1234',
-  }
-  const response = await getEventsByUserName(body)
-  const { data: events } = response
-  // console.log(response)
-
-  /*
-  TODO:
-  - used 'any' in typeScript -> inside .map
-  */
-
   return (
     <>
-      <div className={styles['container-events']}>
-        <Title>{'Mis eventos'}</Title>
-        {/* <Title>{t('title')}</Title> */} {/* error de async */}
-        <main className={styles['events']}>
-          {/* <p>{JSON.stringify(events)}</p> */}
-          {events?.map((event: any) => {
-            return (
-              <UserEvents
-                key={event.id}
-                date={event.createdAt}
-                description={event.description}
-                name={event.name}
-                timeZone={event.timeZone}
-                url={event.url}
-              />
-            )
-          })}
-        </main>
-      </div>
+      <MyEvents />
     </>
   )
 }
