f5d057b74455ea3174955723ca34058660b36af2
equiman <equiman@users.noreply.github.com>
2023-11-01 19:31:19 -0500

    chore(app): :wrench: next configurations

--- i18n.ts
+++
@@ -1,9 +0,0 @@
-import { getRequestConfig } from 'next-intl/server'
-
-export default getRequestConfig(async ({ locale }) => ({
-  // The time zone can either be statically defined, read from the
-  // user profile if you store such a setting, or based on dynamic
-  // request information like the locale or headers.
-  timeZone: 'America/New_York',
-  now: new Date(),
-}))
